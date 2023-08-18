f89e7abfd2acd22a4b27761e621a9b9b52963373
Greg <Jariye05@gmail.com>
2023-08-18 16:48:44 -0700

    updating index.js function

--- src/index.js
+++ src/index.js
@@ -3,20 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import currency from './currency';
 
-function getCurrency(response) {
-  currency.getCurrency(response)
-  .then(response.conversion_rates)
+function getCurrency(USD, otherCurr) {
+  currency.getCurrency(USD, otherCurr)
+  .then(function(response) {
+    if (response.conversion_rates) {
+      
+    }
+  }
 }
-async function getCurrency()
-  const res = await currency.getCurrency(
-    
-  functionResult(response)
-  let result = document.querySelector
-  result.innertext = the ${response.conversion_rate} 
-  function printerror (error)
-)
-functon handleformsubmission(e)
-e.preventdefault()
-
-
-const inputcurr =
